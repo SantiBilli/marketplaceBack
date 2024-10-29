b0fcf867457c798b2e5fb80dfecdead17199d95f
SantiBilli <117537403+SantiBilli@users.noreply.github.com>
2024-10-28 23:06:22 -0300

    paginacion

--- services/stats.js
+++ services/stats.js
@@ -1,12 +1,28 @@
 import { databaseExecute } from "../database/database.js";
 
-export const obtainStatSVC = async (userId) => {
+export const obtainStatSVC = async (userId, page, limit) => {
 
-    const querry = "SELECT videogames.videogameId, videogames.name, videogames.price, videogames.photo, videogames.purchases, videogames.views, videogames.wishlists FROM videogames WHERE userId = ?"
+    let querryArray = [userId]
 
-    const results = await databaseExecute(querry, [userId])
+    let querry = "SELECT videogames.videogameId, videogames.name, videogames.price, videogames.photo, videogames.purchases, videogames.views, videogames.wishlists FROM videogames WHERE userId = ?"
+
+    querry += `LIMIT ? OFFSET ?;`
+    querryArray.push(Number(limit), (page - 1) * limit)
+
+    const results = await databaseExecute(querry, querryArray)
 
     if (!results) return false
     
     return results
+}
+
+export const obtainStatsCountSVC = async (userId) => {
+    
+        const querry = "SELECT COUNT(*) as statsCount FROM videogames WHERE userId = ?"
+    
+        const results = await databaseExecute(querry, [userId])
+    
+        if (!results) return false
+    
+        return results[0].statsCount
 }
